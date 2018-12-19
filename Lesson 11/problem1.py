165924d2d11e92c7fe62a2a19f0f28bdd3be238b
leahpd <44067914+leahpd@users.noreply.github.com>
2018-12-19 12:44:55 -0500

    Add files via upload

---
+++ Lesson 11/problem1.py
@@ -0,0 +1,8 @@
+print('_'*65)
+print('Iz iT BrIcK BoT?')
+temp = input('What is the temperature in Fahrenheit?')
+temp = int(temp)
+if temp < 32:
+	print ('ItZ bRIck, wear ya jacket.')
+else:
+	print ('ItZ not bRIck, enjoy ya day.')
